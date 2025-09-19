b161cc32aa6ffe6ca837b6bad32dc788be3fddc1
AQITPvtLtd <tekbooster25@gmail.com>
2025-09-19 14:11:23 +0530

    new change

--- src/app/verify-otp/page.jsx
+++ src/app/verify-otp/page.jsx
@@ -1,14 +1,12 @@
-"use client"
+"use client";
 
 import React, { Suspense } from "react";
 import VerifyOtpPage from './Verifyotp'
 const page = () => {
     return (
-        <div>
-            <Suspense fallback={<p>Loading...</p>}>
-                <VerifyOtpPage />
-            </Suspense>
-        </div>
+        <Suspense fallback={<p>Loading...</p>}>
+            <VerifyOtpPage />
+        </Suspense>
     )
 }
 
