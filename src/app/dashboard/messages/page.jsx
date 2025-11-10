03ab5b845519f81c9f2e6fa982706ebabe497c4c
AQITPvtLtd <tekbooster25@gmail.com>
2025-11-10 17:55:52 +0530

    add new update

--- src/app/dashboard/messages/page.jsx
+++ src/app/dashboard/messages/page.jsx
@@ -1,10 +1,13 @@
+"use client";
+
 import React from 'react'
+import { Suspense } from "react";
 import MessagesPage from './Messages'
 const page = () => {
     return (
-        <div>
-           <MessagesPage />
-        </div>
+        <Suspense fallback={<div>Loading messages...</div>}>
+            <MessagesPage />
+        </Suspense>
     )
 }
 
